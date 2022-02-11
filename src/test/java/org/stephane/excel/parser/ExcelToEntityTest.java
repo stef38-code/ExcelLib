48ee44064182613baba61c4215ff011f54d206dc
stephane betton <stphane.betton@ag2rlamondiale.fr>
2022-02-11 08:48:28 +0100

    add TU

--- src/test/java/org/stephane/excel/parser/ExcelToEntityTest.java
+++ src/test/java/org/stephane/excel/parser/ExcelToEntityTest.java
@@ -6,6 +6,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.stephane.excel.ExcelException;
+import org.stephane.excel.annotations.ExcelCell;
 import org.stephane.excel.annotations.ExcelDataHeader;
 import org.stephane.excel.annotations.ExcelSheet;
 import org.stephane.excel.entities.Chien;
@@ -31,6 +32,19 @@ class ExcelToEntityTest {
         List<Personne> personnes = excelToEntity.parse(fExcel, Personne.class);
         then(personnes).isNotNull().isNotEmpty();
         personnes.forEach( personne -> assertField(personne));
+    }
+    @Test
+    void parse_numberHeaderSeven_entityPersonne() throws ExcelException {
+        List<Personne3> personnes = excelToEntity.parse(fExcel, Personne3.class);
+        then(personnes).isNotNull().isNotEmpty();
+        personnes.forEach( personne -> {
+            Assertions.assertThat(personne.getName()).isNotNull();
+            Assertions.assertThat(personne.getPostalZip()).isNotNull();
+            Assertions.assertThat(personne.getGuid()).isNotNull();
+            Assertions.assertThat(personne.getAddress()).isNotNull();
+            Assertions.assertThat(personne.getCompany()).isNotNull();
+            Assertions.assertThat(personne.getCity()).isNotNull();
+        });
     }
     @Test
     void parse_fileExcel_entityChien() throws ExcelException {
@@ -69,6 +83,28 @@ class ExcelToEntityTest {
 
         private String city;
 
+        private String guid;
+    }
+
+    @ExcelSheet(name = "feuil2")
+    @ExcelDataHeader(rowNumber = 8)
+    @Getter
+    @Setter
+    private static class Personne3 {
+        public Personne3() {
+        }
+
+        @ExcelCell
+        private String name;
+        @ExcelCell(number =1)
+        private String company;
+        @ExcelCell(number =2)
+        private String address;
+        @ExcelCell(number =3, stringFormat = true)
+        private String postalZip;
+        @ExcelCell(number =4)
+        private String city;
+        @ExcelCell(number =5)
         private String guid;
     }
 }
